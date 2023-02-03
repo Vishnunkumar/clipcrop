3bd0e127f23cae1924ba3930eea37b66701550d8
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-02-03 16:54:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ requirements = [
 
 setuptools.setup(
     name="clipcrop",
-    version="1.0.1",
+    version="1.0.2",
     author="Vishnu Nandakumar",
     author_email="nkumarvishnu25@gmail.com",
     description="Extract sections from your image by using OpenAI CLIP and Facebooks Detr implemented on HuggingFace Transformers",
